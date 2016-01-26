516ecf90f6bce3e34ea1a41b11022c190c8d0f6d
Mancinek <mk.bednarz@gmail.com>
2016-01-26 13:37:17 +0100

    Add score, formatting of code and some little functional changes and comments

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
-// ---------------------------------- SETTINGS ----------------------------------
+// ---------------------------------- GLOBAL SETTINGS ----------------------------------
 
-// define global helper variables for borders of game canvas
+// Defines global helper constants for borders of game canvas
 
-var upperBorder = 60;
-var lowerBorder = 400;
-var rightBorder = 400;
-var leftBorder = 0;
+var UPPER_BORDER = 60;
+var LOWER_BORDER = 400;
+var RIGHT_BORDER = 400;
+var LEFT_BORDER = 0;
 
-// global variables for setting difficulty level - you can adjust:
+// Global variables for setting difficulty level - you can adjust ENEMIES by:
 
-// - number_of_enemies in each line, e.g. number 2 will create 6 enemies (2 enemies in each of 3 lines)
-var number_of_enemies = 2;
+// - numberOfenemies in each line, e.g. number 2 will create 6 enemies (2 enemies in each of 3 lines)
+var numberOfenemies = 2;
 
 // - speed of the enemies,
 var minSpeed = 50;
@@ -25,9 +25,6 @@ var enemyStart = -100;
 // Enemies our player must avoid
 var Enemy = function(x, y, minSpeed, maxSpeed) {
 
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
     //Setting the Enemy initial location - it sets random x location out of screen
     this.x = enemyStart;
     this.y = y;
@@ -40,43 +37,29 @@ var Enemy = function(x, y, minSpeed, maxSpeed) {
     this.sprite = 'images/enemy-bug.png';
 };
 
-
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
 
-    //Updates the Enemy location (you need to implement)
+    // Updates the Enemy location
+    // Parameter: dt - a time delta between ticks
     this.x = this.x + this.speed * dt;
 
     //Going out of screen (right side) - reset the position of enemy to -100 px and sets new speed of enemy between 50 and 300
-    if (this.x>=rightBorder+200) {
-        this.x = -100;
+    if (this.x>=RIGHT_BORDER+200) {
+        this.x = enemyStart;
         this.speed =  Math.floor((Math.random() * maxSpeed) + minSpeed);
-    };
+    }
 
+    // to have the score up to date in the HTML, this function assigns the score id (score in html) to the current score of the player
+    document.getElementById('score').innerHTML = player.score;
 
-    //Handles collision with the Player (you need to implement)
-    /*
-    if (this.x == Player.call(player) {
-        Player();
-    };
-    */
+    // no need to handle collisions with player - enemies are running, overlaping and have different speed
+    // (the speed changes even when the enemy crosses the right border and is reseted)
+
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-//Reset all enemies to the initial position - NIEPOTRZEBNA ????
-
-Enemy.prototype.reset = function() {
-        allEnemies = [];
-        newEnemyLine(2);
 };
 
 
@@ -87,31 +70,24 @@ Enemy.prototype.reset = function() {
 // a handleInput() method.
 
 var Player = function() {
-
-    //Setting the Player initial location
-
+    // Setting the Player initial location
     this.reset();
 
-    //Loading image of player
+    // Setting initial score
+    this.score = 0;
+
+    // Loading image of player
     this.sprite = 'images/char-boy.png';
 };
 
-Player.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+// An update of player position is handled by handleInput function
+// This function only resets the player in case of enemy-player collision
+Player.prototype.update = function() {
 
-    //SIMILIAR TO ENEMY
-
-    this.x * dt;
-    this.y * dt;
-
-    //Collisions - when collide function returns true then plater is reseted
-
+    // Collisions - when collide function returns true then player is reseted
     if (this.collide() === true) {
         this.reset();
     }
-
 };
 
 // Draw the player on the screen, required method for game (THE SAME AS ENEMY)
@@ -125,57 +101,55 @@ Player.prototype.reset = function() {
     this.y = 400;
 };
 
-//The handleInput method, which should receive user input, allowedKeys (the key which was pressed) and move the player according to that input. In particular:
-//Left key should move the player to the left, right key to the right, up should move the player up and down should move the player down.
-//Recall that the player cannot move off screen (so you will need to check for that and handle appropriately).
-//If the player reaches the water the game should be reset by moving the player back to the initial location (you can write a separate reset Player method to handle that).
+// The handleInput method, which receives user input. In particular:
+// Left key should move the player to the left, right key to the right, up should move the player up and down should move the player down
+// The player cannot move off screen
+// If the player reaches the water, the player is back to the initial location and score is increased by 1 point
 Player.prototype.handleInput = function(pressed_key) {
-        if (pressed_key == 'up' && this.y>upperBorder) {
-            this.y = this.y - 85;
-    }   else if (pressed_key === 'up' && this.y<=upperBorder) {
-           this.reset();
-        };
+        if (pressed_key === 'up' && this.y>UPPER_BORDER) {
+            this.y -= 85;
+    }   else if (pressed_key === 'up' && this.y<=UPPER_BORDER) {
+            this.score++;
+            this.reset();
+        }
 
-        if (pressed_key === 'down' && this.y<lowerBorder) {
-            this.y = this.y + 85;
-    };
+        if (pressed_key === 'down' && this.y<LOWER_BORDER) {
+            this.y += 85;
+    }
 
-       if (pressed_key === 'left' && this.x>leftBorder) {
-            this.x = this.x - 100;
-    };
+       if (pressed_key === 'left' && this.x>LEFT_BORDER) {
+            this.x -= 100;
+    }
 
-       if (pressed_key === 'right' && this.x<rightBorder) {
-            this.x = this.x + 100;
-    };
-
+       if (pressed_key === 'right' && this.x<RIGHT_BORDER) {
+            this.x += 100;
+    }
 };
 
 // function defining when collision of player with enemy occurs
 Player.prototype.collide = function() {
     //for each enemy instacnce in allEnemies array a condition is made and when is true it returns TRUE
     for (var i=0; i < allEnemies.length; i++) {
-
         // this condition checks whether the player object's coordinates are within enemies x coordinate (+- 50px) and y cooridinate (+- 30px)
+        // if the player hits the enemy then true is returned and the score is decreased by 1 point (if it is greater than 0)
         if (this.x < allEnemies[i].x + 50 && this.x > allEnemies[i].x - 50 && this.y < allEnemies[i].y + 30 && this.y > allEnemies[i].y - 30) {
+             if (this.score>0) {
+                this.score--;
+            }
             return true;
-            break;
-        };
-
+        }
     }
 };
 
 
-// ---------------------------------- OTHER ----------------------------------
+// ---------------------------------- INSTANCES ----------------------------------
 
-// Now instantiate your objects.
-// Create enemies objects and place all enemy objects in an array called allEnemies
-// Create and place the player object in a variable called player
-
+// Creates enemies objects and place all enemy objects in an array called allEnemies
 var allEnemies = [];
 
-// function NewEnemyLine creates few (number_of_enemies) new objects of Enemy class with coordinates and randomly generated speed for all 3 lines. Then places it in allEnemies array
-var newEnemyLine = function(number_of_enemies) {
-    for (var i=0; i<number_of_enemies; i++) {
+// function NewEnemyLine creates few (numberOfenemies) new instances of Enemy class with coordinates and randomly generated speed for all 3 lines
+var newEnemyLine = function(numberOfenemies) {
+    for (var i=0; i<numberOfenemies; i++) {
         var new_enemy_1 = new Enemy(enemyStart, 60, minSpeed, maxSpeed);
         var new_enemy_2 = new Enemy(enemyStart, 145, minSpeed, maxSpeed);
         var new_enemy_3 = new Enemy(enemyStart, 230, minSpeed, maxSpeed);
@@ -183,14 +157,16 @@ var newEnemyLine = function(number_of_enemies) {
     }
 };
 
-newEnemyLine(number_of_enemies);
+// The creadted enemies are placed in allEnemies array
+newEnemyLine(numberOfenemies);
 
-//creates player object of Player class
+// Creates player instance of Player class
 var player = new Player();
 
 
+// ---------------------------------- KEY LISTENER ----------------------------------
 // This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// Player.handleInput() method
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
